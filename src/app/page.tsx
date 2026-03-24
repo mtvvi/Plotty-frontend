9d29e122747cb22092a90e32b2d9c08dea34205d
andrey57x <57andreys57@gmail.com>
2026-03-24 22:40:30 +0300

    fix 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
 import { StoriesCatalogShell } from "@/widgets/stories/stories-catalog-shell";
+import { AppShellSkeleton } from "@/shared/ui/skeletons/app-shell-skeleton";
 
 export default function HomePage() {
-  return <StoriesCatalogShell />;
+  return (
+    <Suspense fallback={<AppShellSkeleton />}>
+      <StoriesCatalogShell />
+    </Suspense>
+  );
 }
